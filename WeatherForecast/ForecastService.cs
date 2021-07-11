0ff26a86b4e7c7c7f3f5b0f2eda868a5e57f7049
bataisk <donnatroal@gmail.com>
2021-07-12 00:22:28 +0300

    Add WhetherForecastService

---
+++ WeatherForecast/ForecastService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace WeatherForecast
+{
+    public class ForecastService
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly String endpointUrl = "https://goweather.herokuapp.com/weather/";
+
+        public static async Task<IDictionary<string, string>> GetTemperatureForKievAndOdessa()
+        {
+            var kievTemperature = await GetCityTemperature("Kiev");
+            var odessaTemperature = await GetCityTemperature("Odessa");
+            
+            return new Dictionary<string, string>
+            {
+                ["Odessa"] = odessaTemperature,
+                ["Kiev"] = kievTemperature
+            };
+        }
+
+        private static async Task<String> GetCityTemperature(String city)
+        {
+            var forecastResponse = await client.GetAsync(endpointUrl + city);
+            if (!forecastResponse.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            
+            var forecastAsString = await forecastResponse.Content.ReadAsStringAsync();
+            var temperature = JsonDocument.Parse(forecastAsString).RootElement.GetProperty("temperature").GetString();
+
+            return temperature;
+        }
+    }
+}
